bf3f812e10f79a8265b61188c3aa5e33f320b8cc
Alex Sea <seagullalex81@gmail.com>
2025-01-08 17:19:31 +0200

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,12 @@ import { createRoot } from "react-dom/client";
 import "./Index.css";
 import App from "./App.jsx";
 import "modern-normalize";
-import { Toaster } from "react-hot-toast";
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-    <Toaster position="top-right" reverseOrder={false} />
-  </StrictMode>
+  <BrowserRouter>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </BrowserRouter>
 );
