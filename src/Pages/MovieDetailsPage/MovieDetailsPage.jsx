90185dd6015b28c93efa914d7d0e001408dbca55
Alex Sea <seagullalex81@gmail.com>
2025-01-12 21:22:09 +0200

    add Goback

--- src/Pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/Pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -1,10 +1,13 @@
-import { useEffect, useState } from "react";
-import { NavLink, Outlet, useParams } from "react-router-dom";
+import { useEffect, useRef, useState } from "react";
+import { NavLink, Outlet, useLocation, useParams } from "react-router-dom";
 import { getFullDataMovie } from "../../services/api";
 import Loader from "../../components/Loader/Loader";
 import s from "./MovieDetailsPage.module.css";
 
 const MovieDetailsPage = () => {
+  const location = useLocation();
+  const goBackRef = useRef(location.state);
+
   const { moviesId } = useParams();
   const [fullData, setFullData] = useState(null);
 
@@ -27,33 +30,38 @@ const MovieDetailsPage = () => {
   console.log(genres);
 
   return (
-    <div className={s.wrapperMovieDetails}>
-      <img
-        className={s.movieImg}
-        src={`https://image.tmdb.org/t/p/w500/${fullData.poster_path}`}
-        alt={fullData.title}
-      />
+    <>
+      <NavLink to={goBackRef.current || "/"} className={s.goBack}>
+        Go back
+      </NavLink>
+      <div className={s.wrapperMovieDetails}>
+        <img
+          className={s.movieImg}
+          src={`https://image.tmdb.org/t/p/w500/${fullData.poster_path}`}
+          alt={fullData.title}
+        />
 
-      <div className={s.movieContent}>
-        <div className={s.movieText}>
-          <h1>{fullData.title}</h1>
-          <p>{fullData.overview}</p>
-          <p>Rating: {fullData.popularity}</p>
-          <p>Genres: {genres}</p>
+        <div className={s.movieContent}>
+          <div className={s.movieText}>
+            <h1>{fullData.title}</h1>
+            <p>{fullData.overview}</p>
+            <p>Rating: {fullData.popularity}</p>
+            <p>Genres: {genres}</p>
+          </div>
         </div>
-      </div>
 
-      <div className={s.castRewiewsWrapper}>
-        <NavLink to="cast" className={s.navLink}>
-          Cast
-        </NavLink>
-        <NavLink to="reviews" className={s.navLink}>
-          Reviews
-        </NavLink>
-      </div>
+        <div className={s.castRewiewsWrapper}>
+          <NavLink to="cast" className={s.navLink}>
+            Cast
+          </NavLink>
+          <NavLink to="reviews" className={s.navLink}>
+            Reviews
+          </NavLink>
+        </div>
 
-      <Outlet />
-    </div>
+        <Outlet />
+      </div>
+    </>
   );
 };
 
