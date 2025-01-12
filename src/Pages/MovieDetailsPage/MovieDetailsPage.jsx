0f82038d6f3bfeb6b73cdadd566bd54d525d993d
Alex Sea <seagullalex81@gmail.com>
2025-01-12 18:10:52 +0200

    add cast and styles cast

--- src/Pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/Pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { NavLink, Outlet, useParams } from "react-router-dom";
 import { getFullDataMovie } from "../../services/api";
 import Loader from "../../components/Loader/Loader";
+import s from "./MovieDetailsPage.module.css";
 
 const MovieDetailsPage = () => {
   const { moviesId } = useParams();
@@ -19,19 +20,38 @@ const MovieDetailsPage = () => {
 
   if (!fullData) return <Loader />;
 
+  const genres =
+    fullData.genres && fullData.genres.length > 0
+      ? fullData.genres.map((genre) => genre.name).join(", ")
+      : "No genres available";
+  console.log(genres);
+
   return (
-    <div>
-      <h1>{fullData.title}</h1>
+    <div className={s.wrapperMovieDetails}>
       <img
+        className={s.movieImg}
         src={`https://image.tmdb.org/t/p/w500/${fullData.poster_path}`}
         alt={fullData.title}
       />
-      <p>{fullData.overview}</p>
-      <p>Rating: {fullData.popularity}</p>
-      <nav>
-        <NavLink to="cast">Cast</NavLink>
-        <NavLink to="reviews">Reviews</NavLink>
-      </nav>
+
+      <div className={s.movieContent}>
+        <div className={s.movieText}>
+          <h1>{fullData.title}</h1>
+          <p>{fullData.overview}</p>
+          <p>Rating: {fullData.popularity}</p>
+          <p>Genres: {genres}</p>
+        </div>
+      </div>
+
+      <div className={s.castRewiewsWrapper}>
+        <NavLink to="cast" className={s.navLink}>
+          Cast
+        </NavLink>
+        <NavLink to="reviews" className={s.navLink}>
+          Reviews
+        </NavLink>
+      </div>
+
       <Outlet />
     </div>
   );
--- src/components/MovieCast/MovieCast.jsx
+++ src/components/MovieCast/MovieCast.jsx
@@ -1,5 +1,59 @@
+import { useEffect, useState } from "react";
+import { getCast } from "../../services/api";
+import { useParams } from "react-router-dom";
+import Loader from "../Loader/Loader";
+import s from "./MovieCast.module.css";
+
 const MovieCast = () => {
-  return <div>Cast</div>;
+  const { moviesId } = useParams();
+  const [movieCast, setMovieCast] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const getMovieCast = async () => {
+      setIsLoading(true);
+      try {
+        const data = await getCast(moviesId);
+        setMovieCast(data);
+      } catch (error) {
+        console.error("Error fetching movie cast:", error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    if (moviesId) {
+      getMovieCast();
+    }
+  }, [moviesId]);
+
+  return (
+    <div className={s.wrapperActor}>
+      {isLoading ? (
+        <Loader />
+      ) : movieCast && movieCast.length > 0 ? (
+        movieCast.map((actor) => (
+          <div className={s.idActor} key={actor.id}>
+            {actor.profile_path && (
+              <img
+                className={s.imgActor}
+                src={`https://image.tmdb.org/t/p/w200${actor.profile_path}`}
+                alt={actor.name}
+              />
+            )}
+            <div className={s.informActer}>
+              <p className={s.nameActor}>
+                <strong>{actor.name}</strong>
+              </p>
+              <p className={s.characterActor}>Character: {actor.character}</p>
+            </div>
+          </div>
+        ))
+      ) : (
+        <p>No cast available</p>
+      )}
+    </div>
+  );
 };
 
 export default MovieCast;
